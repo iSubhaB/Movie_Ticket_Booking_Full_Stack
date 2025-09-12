face6038d3d90be2dbe11daf1b7c635aa3554281
iSubhaB <suvobiswas.bwn@gmail.com>
2025-09-12 17:52:56 +0530

    First

--- FrontEnd/src/Component/User_Panel/Search_Movie.tsx
+++ FrontEnd/src/Component/User_Panel/Search_Movie.tsx
@@ -5,7 +5,10 @@ export const Search_Movie=()=>{
         <>
        <div>
            <div>
-            <input type="text" name='title' placeholder='Serach Your Movie Here' />
+            <input type="text" name='title' placeholder='Serach Your Movie Here' onChange={handel_Change}/>
+           </div>
+           <div>
+            <h1>Result</h1>
            </div>
        </div>
         </>
